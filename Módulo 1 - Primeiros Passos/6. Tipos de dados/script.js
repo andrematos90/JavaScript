fa4fb768411cd50726224f93176dce24c52c22c2
André Matos <103394184+andrematos90@users.noreply.github.com>
2022-11-10 21:21:58 -0300

    variaveis

---
+++ Módulo 1 - Primeiros Passos/6. Tipos de dados/script.js
@@ -0,0 +1,21 @@
+let nome = "andre " // string
+let idade = 45.5 // decimal
+let logado = false // booleano
+
+
+ // variavel do tipo "array", que é uma lista que pode receber strings, decimais, objetos, etc..
+let lista = [   
+    "trigo",
+    "leite",
+    "agua",
+    "farinha "
+]
+
+
+ // é como se fosse uma lista, um array, mas com um "indice" ou "referncia"
+
+
+let nomeCompleto = { nome: "andre", idade: 32}       
+
+
+console.log(typeof nome);  //typeof retornao tipo da variavel
---
+++ Módulo 1 - Primeiros Passos/8. Exercicio de Variáveis/script.js
@@ -0,0 +1,11 @@
+let carro = "Ferrari"
+ 
+console.log(carro);
+
+let precoBolo = 19.99
+
+console.log("O valor do bolo é " + '' + "R$" + precoBolo)
+
+
+let  cidade = "São Paulo"
+console.log(cidade);
