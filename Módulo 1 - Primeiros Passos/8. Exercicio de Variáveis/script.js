c6249d7b69456f8291d27c31d86531c2c469ea32
André Matos <andrermatos90@gmail.com>
2023-06-05 22:56:59 -0300

    exercicio váriaveis

--- Módulo 1 - Primeiros Passos/8. Exercicio de Variáveis/script.js
+++ Módulo 1 - Primeiros Passos/8. Exercicio de Variáveis/script.js
@@ -9,3 +9,20 @@ console.log("O valor do bolo é " + '' + "R$" + precoBolo)
 
 let  cidade = "São Paulo"
 console.log(cidade);
+
+/*
+//1. crie uma variável para armazenar o nome do carro de ponha o valor "FERRARI". E exiba-a no console
+
+let carro =  'ferrari'
+console.log(carro)
+
+//2. crie uma variável para armazenar o preço de um bolo e exiba
+
+let bolo = {'preco' : 8.99 }
+console.log(bolo)
+
+//3. corrija o código a seguir e exiba-o let "cidade" = São Paulo
+
+let cidade = "São Paulo"
+console.log(cidade)
+*/
--- Testes.js
+++ Testes.js
@@ -1,5 +1,14 @@
-let nome = "andré"
-let idade = 33
+//1. crie uma variável para armazenar o nome do carro de ponha o valor "FERRARI". E exiba-a no console
 
-alert(nome)
-alert(idade)
+let carro =  'ferrari'
+console.log(carro)
+
+//2. crie uma variável para armazenar o preço de um bolo e exiba
+
+let bolo = {'preco' : 8.99 }
+console.log(bolo)
+
+//3. corrija o código a seguir e exiba-o let "cidade" = São Paulo
+
+let cidade = "São Paulo"
+console.log(cidade)
