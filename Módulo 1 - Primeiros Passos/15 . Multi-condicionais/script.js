f005f9c954726976fad37294334d793d627a7b96
André Matos <andrermatos90@gmail.com>
2023-06-07 23:24:57 -0300

    multi-condicionais

--- Módulo 1 - Primeiros Passos/15 . Multi-condicionais/script.js
+++ Módulo 1 - Primeiros Passos/15 . Multi-condicionais/script.js
@@ -6,3 +6,17 @@ if( idade >= 18 && idade <= 60 ){ //multi-condicionais
 else { 
     console.log("Véio");
 }
+
+/* 
+let idade = 89
+
+if ((idade >= 18) && (idade < 60)){
+    console.log(`com ${idade} você é adulto!`)
+}
+else if (idade < 18 ){
+    console.log(`com ${idade} você é jovem`)
+}
+else {
+    console.log(`com ${idade} você é idoso`)
+}
+*/
--- Testes.js
+++ Testes.js
@@ -1,9 +1,11 @@
-let idade = 4
+let idade = 89
 
-if ( idade >= 18)
-{
-    console.log('Maior de Idade!')
+if ((idade >= 18) && (idade < 60)){
+    console.log(`com ${idade} você é adulto!`)
 }
-   
-else
-   {console.log('menor de idade')}
+else if (idade < 18 ){
+    console.log(`com ${idade} você é jovem`)
+}
+else {
+    console.log(`com ${idade} você é idoso`)
+}
