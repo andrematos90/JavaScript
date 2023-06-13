143094ff241561c54a764f71ee33528b25312c4c
André Matos <andrermatos90@gmail.com>
2023-06-12 21:02:36 -0300

    o que são objetos

--- Testes.js
+++ Testes.js
@@ -1,7 +1,19 @@
-let lista = ['pao', 'ovo', 'peru', 'largato']
-lista[0] = 'leite'
-lista.pop()
-lista.push('chocolate') 
-let quantidade_itens = lista.length
-console.log(quantidade_itens)
-console.log(lista)
+// 1. No array abaixo qual numero pega a ferrari
+let carros = ['bmw', 'ferrari', 'mercedes']
+console.log(`o item que pega a ${carros[1]} é o "1"`)
+
+//2. troque a ferrari por "audi"
+
+carros[1] = 'audi'
+console.log('Lista com "audi":')
+console.log(carros)
+
+//3.adicione "volvo" a lista
+
+carros.push('volvo')
+console.log('Lista com "volvo" no final:')
+console.log(carros)
+
+//4.exiba quantos itens contém o array 
+
+console.log(`Quantidade de itens: ${carros.length}`)
