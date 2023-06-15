f9fb2b39915f4bcfe5dcb698ab2a61d1205a8638
André Matos <andrermatos90@gmail.com>
2023-06-14 21:42:05 -0300

    ex 002 - Variáveis

---
+++ Exercicios/ex 002.js
@@ -0,0 +1,23 @@
+/*
+Escreva um programa em JavaScript que solicite ao usuário que insira seu nome, idade e cidade de residência. Em seguida, armazene essas informações em variáveis apropriadas e exiba-as no console.*/ 
+
+let nome = prompt('Nome: ')
+let idade = prompt('Idade: ')
+let cidade = prompt('Cidade: ')
+
+console.log(`Nome: ${nome}`)
+console.log(`Idade: ${idade}`)
+console.log(`Cidade: ${cidade}`)
+
+/*
+A primeira linha de código declara uma variável chamada nome e utiliza o comando prompt() para exibir uma caixa de diálogo solicitando ao usuário que insira seu nome. O valor inserido pelo usuário é armazenado na variável nome.
+
+A segunda linha declara uma variável chamada idade e utiliza o comando prompt() para solicitar ao usuário que insira sua idade. O valor inserido pelo usuário é armazenado na variável idade.
+
+A terceira linha declara uma variável chamada cidade e utiliza o comando prompt() para solicitar ao usuário que insira sua cidade de residência. O valor inserido pelo usuário é armazenado na variável cidade.
+
+A quarta linha utiliza o comando console.log() para exibir uma mensagem no console, que inclui o valor armazenado na variável nome.
+
+A quinta linha utiliza o comando console.log() para exibir uma mensagem no console, que inclui o valor armazenado na variável idade.
+
+A sexta linha utiliza o comando console.log() para exibir uma mensagem no console, que inclui o valor armazenado na variável cidade. */
