6b12838a4f6367eb9b56bdae877c86a348d7d673
André Matos <andrermatos90@gmail.com>
2023-06-14 22:14:41 -0300

    ex 004 - operações com variáveis

---
+++ Exercicios/ex 004.js
@@ -0,0 +1,27 @@
+/*
+Escreva um programa em JavaScript que declare duas variáveis numéricas e realize as seguintes operações com elas: adição, subtração, multiplicação, divisão e resto da divisão (ou módulo). Em seguida, exiba os resultados dessas operações no console. */
+
+let numero1 = 4
+let numero2 = 100
+
+console.log(`${numero1} + ${numero2} = ${numero1 + numero2}`)
+console.log(`${numero1} - ${numero2} = ${numero1 - numero2}`)
+console.log(`${numero1} x ${numero2} = ${numero1 * numero2}`)
+console.log(`${numero1} / ${numero2} = ${numero1 / numero2}`)
+console.log(`Resto da divisão de ${numero1} dividido por ${numero2} = ${numero1 % numero2}`)
+
+
+/* A primeira linha de código declara uma variável chamada numero1 e atribui a ela o valor 4.
+
+A segunda linha de código declara uma variável chamada numero2 e atribui a ela o valor 100.
+
+A terceira linha utiliza o comando console.log() para exibir uma mensagem no console que realiza a adição dos valores das variáveis numero1 e numero2.
+
+A quarta linha utiliza o comando console.log() para exibir uma mensagem no console que realiza a subtração dos valores das variáveis numero1 e numero2.
+
+A quinta linha utiliza o comando console.log() para exibir uma mensagem no console que realiza a multiplicação dos valores das variáveis numero1 e numero2.
+
+A sexta linha utiliza o comando console.log() para exibir uma mensagem no console que realiza a divisão dos valores das variáveis numero1 e numero2.
+
+A sétima linha utiliza o comando console.log() para exibir uma mensagem no console que calcula o resto da divisão dos valores das variáveis numero1 e numero2.
+*/
