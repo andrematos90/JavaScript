8c5a07784a5caeb0726aa629351307b92717bda4
André Matos <andrermatos90@gmail.com>
2023-06-15 16:20:33 -0300

    ex 013 - arrow function

---
+++ Exercicios/ex 013.js
@@ -0,0 +1,17 @@
+/*Escreva uma função de seta (arrow function) que receba um número como parâmetro e retorne o dobro desse número.*/
+
+const dobro = (numero) => console.log(numero * 2);
+
+n = prompt('digite o número que equer saber o dobro: '),
+dobro(n);
+
+/*
+É declarada uma variável constante chamada dobro.
+A variável dobro é atribuída a uma função de seta (arrow function) que recebe um parâmetro chamado numero.
+Dentro da função dobro, o parâmetro numero é multiplicado por 2.
+O resultado da multiplicação é exibido no console usando console.log().
+É solicitado ao usuário que digite um número através da função prompt().
+O valor digitado pelo usuário é armazenado na variável n.
+A função dobro é chamada, passando o valor de n como argumento.
+A função dobro calcula o dobro do número passado como argumento e o exibe no console.*/
+
