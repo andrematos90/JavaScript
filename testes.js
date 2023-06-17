9365feb45dc71505ad295f5016b34b1b2c82bcbc
André Matos <andrermatos90@gmail.com>
2023-06-16 22:23:17 -0300

    funções de arrays

---
+++ testes.js
@@ -0,0 +1,15 @@
+let fruits = [ 'maça', 'uva', 'morando', 'banana', 'limão']
+
+for(let fruit in fruits){
+    console.log(fruits[fruit])
+}
+
+for (fruit of fruits){
+    console.log(fruit)
+}
+
+let n = 1
+while (n <= 100){
+    console.log(n)
+    n++
+}
