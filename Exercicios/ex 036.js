b1c4b5908311bb1539f8ac3865158d91156fb73f
André Matos <andrermatos90@gmail.com>
2023-06-20 22:26:27 -0300

    manipulando elementos

---
+++ Exercicios/ex 036.js
@@ -0,0 +1,36 @@
+/*
+Você foi contratado para desenvolver uma funcionalidade de contador de cliques para um site. A cada vez que o usuário clicar em um botão específico, o contador deve ser incrementado em 1 e exibido na tela. Para isso, você deve utilizar eventos de clique em JavaScript.
+
+Instruções:
+
+Crie um arquivo HTML contendo um botão com o ID "botaoContador" e um elemento de texto com o ID "contador".
+Defina uma variável chamada cliques com o valor inicial de 0.
+Implemente uma função em JavaScript chamada atualizarContador(), que atualizará o valor exibido no elemento de texto com o valor da variável cliques.
+Adicione um evento de clique ao botão com o ID "botaoContador".
+Na função de evento, incrementa a variável cliques em 1 e chame a função atualizarContador().
+Teste o contador clicando no botão e verificando se o valor exibido é incrementado a cada clique.
+Observações:
+
+Utilize o método getElementById() para obter referências aos elementos HTML com os IDs "botaoContador" e "contador".
+Certifique-se de que o evento de clique está sendo adicionado corretamente ao botão.
+A função atualizarContador() deve atualizar o texto do elemento de texto com o ID "contador" para refletir o valor atual da variável cliques.
+Utilize apenas HTML, CSS e JavaScript para implementar a solução.*/
+
+
+let cliques = 1
+
+function atualizarContador(){
+    let elemento = document.getElementById("contador")
+    console.log(elemento)
+    elemento.textContent = cliques++
+
+}
+
+/*
+let elemento = document.getElementById("contador"): Essa linha busca o elemento do DOM com o ID "contador" e o armazena na variável elemento.
+
+console.log(elemento): Essa linha exibe o elemento no console do navegador. Isso é útil para verificar se o elemento está sendo corretamente obtido.
+
+elemento.textContent = cliques++: Nesta linha, a propriedade textContent do elemento é atualizada com o valor de cliques. O operador ++ é usado para incrementar o valor de cliques em 1 após atribuí-lo ao textContent. Isso garante que o valor exibido seja atualizado a cada vez que a função atualizarContador() é chamada.
+
+Portanto, quando a função atualizarContador() é chamada, o valor atual de cliques é exibido no elemento com o ID "contador" e, em seguida, é incrementado em 1 para o próximo clique.*/
---
+++ Módulo 3 - Eventos e manipulação de DOM/5. Manipulando elementos 1/script.js
@@ -0,0 +1,25 @@
+function clicou(){
+    let teste = document.querySelector('#teste');
+    console.log(teste.children[0].children);
+    const ul = teste.querySelector('ul');
+
+    ul.innerHTML = "<li> Item alterado</li>";
+
+    console.log(ul.innerHTML);
+}
+
+/*
+A função clicou() começa obtendo uma referência para um elemento HTML com o ID "teste" usando o método querySelector. O elemento encontrado é armazenado na variável teste.
+
+Em seguida, é feito um console.log(teste.children[0].children). Vamos analisar isso em partes:
+
+teste.children[0] retorna o primeiro filho do elemento teste. Essa notação [0] é usada para acessar o primeiro elemento na lista de filhos.
+.children retorna uma lista dos filhos do primeiro filho de teste. Isso imprimirá a lista de elementos filhos.
+A próxima linha de código utiliza teste.querySelector('ul') para encontrar o primeiro elemento <ul> dentro do elemento teste. O resultado é armazenado na constante ul.
+
+A linha ul.innerHTML = "<li> Item alterado</li>"; define o conteúdo HTML da lista <ul> para uma nova string, que neste caso é "<li> Item alterado</li>". Isso substituirá todo o conteúdo anterior da lista.
+
+Por fim, é exibido no console o conteúdo atualizado da lista usando console.log(ul.innerHTML).
+
+Em resumo, a função clicou() obtém uma referência para um elemento HTML com o ID "teste". Em seguida, ela procura o primeiro elemento <ul> dentro desse elemento e altera o seu conteúdo para uma única <li> com o texto "Item alterado".
+*/
