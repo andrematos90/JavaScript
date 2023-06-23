5e13d541c4f9da53c3270db2432d4c4da3138370
André Matos <andrermatos90@gmail.com>
2023-06-22 23:27:30 -0300

    ex 042 nao feito

---
+++ Exercicios/ex 042.js
@@ -0,0 +1,12 @@
+/*Crie uma função em JavaScript que permita a manipulação de atributos no DOM. A função deve receber dois parâmetros: o primeiro será o seletor do elemento que se deseja manipular, e o segundo será o nome do atributo que se deseja modificar. O objetivo da função é realizar as seguintes ações:
+
+Verificar se o elemento com o seletor fornecido existe no DOM.
+Se o elemento existir, verificar se ele possui o atributo especificado.
+Se o atributo existir no elemento, solicitar ao usuário um novo valor para atribuí-lo.
+Após obter o novo valor do atributo, atualizá-lo no elemento.
+Se o elemento não existir ou não possuir o atributo especificado, exibir uma mensagem de erro correspondente.*/
+
+
+function alteraAtributos(seletor, nomedoAtributo){
+    
+}
