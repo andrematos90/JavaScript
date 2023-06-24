efe9a5a2d286ef019c06d6686b92f022f7615ab0
André Matos <andrermatos90@gmail.com>
2023-06-24 10:54:27 -0300

    ex 042 - manipulação de atributos

--- Exercicios/ex 042.js
+++ Exercicios/ex 042.js
@@ -1,12 +1,28 @@
-/*Crie uma função em JavaScript que permita a manipulação de atributos no DOM. A função deve receber dois parâmetros: o primeiro será o seletor do elemento que se deseja manipular, e o segundo será o nome do atributo que se deseja modificar. O objetivo da função é realizar as seguintes ações:
+/*Neste exercício, você será desafiado a selecionar e manipular atributos de elementos no DOM (Document Object Model) usando JavaScript. Você deve implementar as seguintes tarefas:
 
-Verificar se o elemento com o seletor fornecido existe no DOM.
-Se o elemento existir, verificar se ele possui o atributo especificado.
-Se o atributo existir no elemento, solicitar ao usuário um novo valor para atribuí-lo.
-Após obter o novo valor do atributo, atualizá-lo no elemento.
-Se o elemento não existir ou não possuir o atributo especificado, exibir uma mensagem de erro correspondente.*/
+Selecione um elemento HTML com o ID "meuElemento".
+Obtenha o valor do atributo "src" de uma imagem.
+Altere o valor do atributo "href" de um link para "https://www.exemplo.com".
+Verifique se um botão possui o atributo "disabled" e exiba uma mensagem informando se está habilitado ou desabilitado.
+Remova o atributo "class" de um elemento.
+*/
 
+var image = document.getElementById('meuElemento');
+var src = image.getAttribute('src');
+console.log(src)
 
-function alteraAtributos(seletor, nomedoAtributo){
-    
+var link = document.querySelector('a');
+link.setAttribute('href', 'https://www.exemplo.com');
+console.log(link)
+
+var botao = document.getElementById('botao');
+if (botao.hasAttribute('disabled')){
+    console.log('possui atributo disable');
 }
+else{
+    console.log('Não possui atributo');
+}
+
+var div = document.querySelector('.minhadiv');
+div.removeAttribute('class');
+
