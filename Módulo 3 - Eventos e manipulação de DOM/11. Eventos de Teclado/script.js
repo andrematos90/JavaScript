9da8b1cedfdb96ab182beeb157d5aae506be733f
André Matos <andrermatos90@gmail.com>
2023-06-26 22:49:19 -0300

    Eventos de Teclado

---
+++ Módulo 3 - Eventos e manipulação de DOM/11. Eventos de Teclado/script.js
@@ -0,0 +1,16 @@
+function apertou(){
+    console.log('Apertou!')
+
+}
+
+function segurou(){
+    console.log('Segurou')
+
+}
+
+function soltou(){
+    console.log('soltou')
+
+}
+
+document.addEventListener('keyup', soltou);
