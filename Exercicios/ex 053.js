6dfc0e28dab6d538347b8849ab3e5a1e4a15e053
André Matos <andrermatos90@gmail.com>
2023-06-30 08:53:02 -0300

    ex 053

---
+++ Exercicios/ex 053.js
@@ -0,0 +1,24 @@
+/*
+Você foi contratado para desenvolver uma calculadora de média ponderada. Sua tarefa é criar uma função chamada calcularMediaPonderada que recebe um array de objetos, onde cada objeto possui duas propriedades: nota e peso. A função deve calcular e retornar a média ponderada das notas, levando em consideração os pesos fornecidos.
+
+Requisitos do exercício:
+
+Crie uma função chamada calcularMediaPonderada que recebe um parâmetro chamado notas (um array de objetos).
+Dentro da função calcularMediaPonderada, crie uma função interna chamada calcularProduto que recebe um parâmetro chamado nota e retorna o produto da nota pelo peso.
+Utilize a função reduce() para percorrer o array de objetos e calcular a soma dos produtos das notas pelos pesos.
+Utilize a função reduce() novamente para percorrer o array de objetos e calcular a soma dos pesos.
+Divida a soma dos produtos pela soma dos pesos para obter a média ponderada.
+Retorne a média ponderada calculada pela função calcularMediaPonderada.*/
+
+
+function calcularMediaPonderada(notas){
+    function calcularProduto(nota){
+
+    }
+}
+
+notas = [
+    { peso: 1, nota: 8 },
+    {peso: 2, nota: 7 },
+    {peso: 1, nota: 6},
+]
