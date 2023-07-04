49bf751d1e6693a3b404fa3f7d6efda3a2b6f1cc
André Matos <andrermatos90@gmail.com>
2023-07-03 22:07:58 -0300

    ex 062.js

---
+++ Exercicios/ex 062.js
@@ -0,0 +1,26 @@
+/*
+Considere o seguinte objeto "pessoa":
+
+let pessoa = {
+  nome: "João",
+  idade: 25,
+  profissao: "Engenheiro"
+};
+
+Acesse a propriedade "idade" do objeto "pessoa" e armazene o valor em uma variável chamada "idadeAtual".
+Altere a propriedade "profissao" do objeto "pessoa" para "Desenvolvedor".
+Adicione uma nova propriedade chamada "cidade" ao objeto "pessoa" e atribua o valor "São Paulo".
+
+*/
+
+let pessoa = {
+    nome: "João",
+    idade: 25,
+    profissao: "Engenheiro" 
+}
+
+idadeAtual =  pessoa['idade']
+pessoa['profissao'] = 'Desenvolvedor'
+pessoa ['cidade'] = 'Sao Paulo'
+
+console.log(pessoa)
