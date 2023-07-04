da733cf1182f92c80532686f177e65410267bbf0
André Matos <andrermatos90@gmail.com>
2023-07-03 22:23:52 -0300

    ex 061 - forEach

--- Exercicios/ex 061.js
+++ Exercicios/ex 061.js
@@ -16,5 +16,4 @@ Dentro da função de callback, temos a seguinte lógica:
 
 console.log(element) imprime cada elemento no console.
 Dessa forma, a função de callback é executada para cada elemento do array, imprimindo cada nome no console.
-
 O método forEach não retorna nenhum valor. Ele é usado quando você precisa realizar uma ação em cada elemento do array, como a impressão no console, mas não precisa de um resultado específico.*/
