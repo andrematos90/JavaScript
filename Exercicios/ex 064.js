12a1aa16d14fc4eeb8fcec4e5a61c16cd07c2eb4
André Matos <andrermatos90@gmail.com>
2023-07-03 22:45:12 -0300

    ex 064 - metodo pop

---
+++ Exercicios/ex 064.js
@@ -0,0 +1,16 @@
+/*
+Considere o seguinte array "frutas":
+
+let frutas = ["maçã", "banana", "laranja", "uva"];
+
+Utilizando o método pop(), remova o último elemento do array "frutas".
+Armazene o elemento removido em uma variável chamada "frutaRemovida".
+Imprima a variável "frutaRemovida" no console.
+Execute o código e verifique se o elemento removido foi impresso corretamente no console.*/
+
+let frutas = ["maçã", "banana", "laranja", "uva"];
+
+frutaRemovida = frutas.pop();
+console.log(frutaRemovida)
+
+/* O código utiliza o método pop() para remover o último elemento do array "frutas" e armazena o elemento removido na variável "frutaRemovida". Em seguida, o valor da variável "frutaRemovida" é impresso no console utilizando o método console.log().*/
