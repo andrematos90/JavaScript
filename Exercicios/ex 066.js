c292b39b636479ec8db30b9e039dd3944bd5f548
André Matos <andrermatos90@gmail.com>
2023-07-05 09:29:24 -0300

    ex 066

---
+++ Exercicios/ex 066.js
@@ -0,0 +1,21 @@
+/* 
+Crie uma página HTML simples contendo uma lista não ordenada <ul> com alguns itens <li>. Em seguida, utilize JavaScript para implementar as seguintes tarefas:
+
+Acessar o elemento de lista não ordenada <ul> através do seu ID e armazená-lo em uma variável.
+
+Acessar o primeiro item da lista <li> e armazená-lo em uma variável.
+
+Modificar o conteúdo do primeiro item da lista para "Item 1 modificado" usando JavaScript.
+
+Criar um novo elemento <li> e adicionar um texto qualquer como conteúdo.
+
+Adicionar o novo elemento <li> criado à lista <ul> existente.
+
+Remover o segundo item da lista <li>.
+
+Alterar a cor do texto de todos os itens da lista para vermelho.
+
+Ao clicar em um item da lista, exibir um alert com o texto desse item.*/
+
+var elemento = document.querySelector("#ordenada")
+console.log(elemento);
