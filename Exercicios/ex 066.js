093e3c8aae92329366c6e67918305a4f0f5cb8bc
André Matos <andrermatos90@gmail.com>
2023-07-05 22:19:52 -0300

    ex 066 - acessando e manipulando elementos

--- Exercicios/ex 066.js
+++ Exercicios/ex 066.js
@@ -17,5 +17,42 @@ Alterar a cor do texto de todos os itens da lista para vermelho.
 
 Ao clicar em um item da lista, exibir um alert com o texto desse item.*/
 
-var elemento = document.querySelector("#ordenada")
-console.log(elemento);
+
+
+//Acessar o elemento de lista não ordenada <ul> através do seu ID e armazená-lo em uma variável.
+var lista = document.getElementById("myList");
+
+//Acessar o primeiro item da lista <li> e armazená-lo em uma variável.
+var primeiroLi = document.querySelector("li")
+
+//Modificar o conteúdo do primeiro item da lista para "Item 1 modificado" usando JavaScript.
+primeiroLi.innerText = 'Item 1 (modificado)'
+
+//Criar um novo elemento <li> e adicionar um texto qualquer como conteúdo.
+lista.innerHTML = lista.innerHTML + '<li>Novo Item</li>';
+
+
+//Remover o segundo item da lista <li>.
+lista.children[1].remove();
+
+// Alterar a cor do texto de todos os itens da lista para vermelho.
+lista.style.color = '#F00'
+
+//Ao clicar em um item da lista, exibir um alert com o texto desse item.
+
+function clicado() {
+    elemento = document.getElementById('myList');
+
+    elemento.addEventListener("click", function(event) {
+        // Verificando o elemento de origem (target) do evento de clique
+        var elementoClicado = event.target;
+
+        // Manipulando o elemento clicado
+       
+        alert(elementoClicado.innerText);
+    });
+
+}
+
+//chamando a função
+clicado();
---
+++ Exercicios/testes.js
@@ -0,0 +1,6 @@
+function clicou(){
+    console.log('clicou')
+}
+
+let botao = document.getElementById('botao');
+botao.addEventListener('click', clicou);
