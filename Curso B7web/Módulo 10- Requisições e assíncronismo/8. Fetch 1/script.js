28675efb3ad3a94c7654fe0b2846a3eed5bf255d
André Matos <andrermatos90@gmail.com>
2023-07-17 21:12:03 -0300

    promises

--- Curso B7web/Módulo 10- Requisições e assíncronismo/8. Fetch 1/script.js
+++ Curso B7web/Módulo 10- Requisições e assíncronismo/8. Fetch 1/script.js
@@ -4,6 +4,7 @@ por exemplo o metodo "get","post","put","delete"
 se nada for colocado no segundo parametro ele vai fazer a requisição "get"
 */
 
+/*
 
 function loadPosts(){
     document.getElementById("posts").innerHTML = 'carregando .....';
@@ -20,3 +21,28 @@ function loadPosts(){
      });
                                                                     
 }  
+
+
+*/
+
+const userName = 'andrematos90';
+
+fetch(`https://api.github.com/users/${userName}`, {
+
+
+   method: 'GET',
+   headers: {
+      Accpet: 'application/vnd.github.v3+json',
+   },
+})
+.then((response) => {
+   console.log(typeof response)
+   console.log(response)
+   return response.json()
+})
+.then((data) => {
+   console.log(`Usuário: ${data}`)
+})
+cath((erro) => {
+   console.log(`ERRO! ${erro}`)
+})
