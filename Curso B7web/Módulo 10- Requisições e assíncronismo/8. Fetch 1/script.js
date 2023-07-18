05a127c6a0ba1db0d861c54bd0fccad6a486ae82
André Matos <andrermatos90@gmail.com>
2023-07-18 20:57:12 -0300

    fetch

--- Curso B7web/Módulo 10- Requisições e assíncronismo/8. Fetch 1/script.js
+++ Curso B7web/Módulo 10- Requisições e assíncronismo/8. Fetch 1/script.js
@@ -4,46 +4,20 @@ por exemplo o metodo "get","post","put","delete"
 se nada for colocado no segundo parametro ele vai fazer a requisição "get"
 */
 
-/*
+function loadPosts() {
 
-function loadPosts(){
-    document.getElementById("posts").innerHTML = 'carregando .....';
-    fetch('https://jsonplaceholder.typicode.com/posts')   
-     .then(function(resultado){
-        return resultado.json();
-       
-     })   
-     .then(function(json){
-        document.getElementById("posts").innerHTML = json.length+' posts';
-     })        
-     .catch(function(error){
-        console.log("erro!");
-     });
-                                                                    
-}  
+   document.getElementById('posts').innerHTML = 'carregando...';
+   
+   fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+         return response.json();
+      })
+      .then(json => {
+         document.getElementById('posts').innerHTML = json.length + 'posts';
+      })
+      .catch(error => {
+         console.log("se fudeu")
+      })
 
 
-*/
-
-const userName = 'andrematos90';
-
-fetch(`https://api.github.com/users/${userName}`, {
-
-
-
-   method: 'GET',
-   headers: {
-      Accpet: 'application/vnd.github.v3+json',
-   },
-})
-.then((response) => {
-   console.log(typeof response)
-   console.log(response)
-   return response.json()
-})
-.then((data) => {
-   console.log(`Usuário: ${data}`)
-})
-cath((erro) => {
-   console.log(`ERRO! ${erro}`)
-})
+}
