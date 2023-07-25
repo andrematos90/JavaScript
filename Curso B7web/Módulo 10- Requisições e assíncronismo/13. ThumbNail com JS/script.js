1ad15de70fff7c5386307fe4496088cccca6a8a1
André Matos <andrermatos90@gmail.com>
2023-07-25 16:08:06 -0300

    thumbnail com js

--- Curso B7web/Módulo 10- Requisições e assíncronismo/13. ThumbNail com JS/script.js
+++ Curso B7web/Módulo 10- Requisições e assíncronismo/13. ThumbNail com JS/script.js
@@ -1,12 +1,18 @@
+
 async function mostrar(){
-   let imagem = document.getElementById('imagem').files[0];  //seleciona a imagem 
-
-  //apartir do objeto "file" gera apartir dele uma URL', temos duas opções criar a imagem na lata  ex: inner.HTML = <img />; e depois altero essa imagem
+   // Obtém o elemento de input do tipo file com o id "imagem" e seleciona o primeiro arquivo (caso múltiplos arquivos possam ser selecionados)
+   let imagem = document.getElementById('imagem').files[0];
+ 
+   // Cria um elemento <img> para exibir a imagem na página
+   let img = document.createElement('img');
+ 
+   // Define a propriedade 'src' do elemento <img> como a URL do objeto 'imagem' usando o método 'createObjectURL'
+   // O método 'createObjectURL' gera uma URL temporária para o objeto Blob ou File, neste caso, para a imagem selecionada
+   img.src = URL.createObjectURL(imagem);
+ 
+   // temos duas opções criar a imagem  ex: inner.HTML = <img />; e depois altero essa imagem
   // ou posso criar a tag da imagem e adiciono
-  let img = document.createElement('img');
-  img.src = URL.createObjectURL(imagem) // passo como parametro a imagem/file
-
+   //Adiciona o elemento <img> à área com o id "area" na página (a imagem será exibida na página)
    document.getElementById('area').appendChild(img);
-}
-
-
+ }
+ 
