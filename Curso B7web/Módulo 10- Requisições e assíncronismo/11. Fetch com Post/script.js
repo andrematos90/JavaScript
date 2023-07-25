6ac5f7dee77285eced9fb85358e9b45504305379
André Matos <andrermatos90@gmail.com>
2023-07-25 14:14:40 -0300

    fetch com post

--- Curso B7web/Módulo 10- Requisições e assíncronismo/11. Fetch com Post/script.js
+++ Curso B7web/Módulo 10- Requisições e assíncronismo/11. Fetch com Post/script.js
@@ -1,19 +1,25 @@
 async function inserirPost(){
+   // Define o conteúdo da div com o id "posts" como "carregando....."
    document.getElementById("posts").innerHTML = "carregando.....";
-
-   let req = await fetch('http://jsonplaceholder.typicode.com/posts',{
-      method: 'POST',  // tipo da requisição
-      body: JSON.stringify({
-         title:'Titulo e teste',   // esses sao os dados que quero enviar, envio para o stringfy, que transforma o objeto em string com o objeto dentro
-         body: 'Corpo de Teste',
-         useId: 4
-      }), 
-      headers: {    // esse é o cabeçalho
-         'Content-Type': 'application/json'
-      }
+ 
+   // Faz uma requisição POST para a URL 'http://jsonplaceholder.typicode.com/posts'
+   // O corpo da requisição contém um objeto JSON com os campos 'title', 'body', e 'userId'
+   // O cabeçalho 'Content-Type' é definido como 'application/json'
+   let req = await fetch('https://jsonplaceholder.typicode.com/posts',{
+     method: 'POST',  
+     body: JSON.stringify({
+        title:'Titulo e teste', 
+        body: 'Corpo de Teste',
+        useId: 4
+     }), 
+     headers: {   
+        'Content-Type': 'application/json'
+     }
    });
-
-   let json = await req.json();  // recebo o resultado
-
-   console.log(json);  // mostro no console
-}
+ 
+   // Aguarda a resposta da requisição ser resolvida como JSON
+   let json = await req.json(); 
+ 
+   // Imprime a resposta JSON no console
+   console.log(json);  
+ }
