706dbdd5f8a7c07f4ceb157ebabb191cd2f1cd4e
André Matos <andrermatos90@gmail.com>
2023-07-26 15:21:35 -0300

    aula 01

--- CFBCursos/Curso de Javascript Moderno - Aula 01/script.js
+++ CFBCursos/Curso de Javascript Moderno - Aula 01/script.js
@@ -1,1 +1,7 @@
+const fs = require('fs');
+const path = require('path')
+
+fs.readdir(path.join('C:\Users\andre\OneDrive\Documentos\GitHub\JavaScript\CFBCursos\Curso de Javascript Moderno - Aula 01'))
+.then((files) => {console.log(files)})
+
 console.log('Olá mundo de novo!');
