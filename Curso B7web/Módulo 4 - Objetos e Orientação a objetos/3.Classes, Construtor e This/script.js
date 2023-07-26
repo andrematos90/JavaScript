dd08aac3b241f65e8c178e7fd2dd5b4e5c905aec
André Matos <andrermatos90@gmail.com>
2023-07-26 20:46:16 -0300

    construtor e this

---
+++ Curso B7web/Módulo 4 - Objetos e Orientação a objetos/3.Classes, Construtor e This/script.js
@@ -0,0 +1,9 @@
+class Person {
+//constructor é a função que seraá executada quano um novo objeto for instrnaciado
+    constructor(name, age){
+        //this se refere ao proprio objeto que esta sendo criado
+        this.name = name;   //atributo name recebe a variável "name"
+        this.age = age //  os atributos name/age não precisariam  ser o mesmo nome das váriaveis  "name e age"
+    }
+}
+
