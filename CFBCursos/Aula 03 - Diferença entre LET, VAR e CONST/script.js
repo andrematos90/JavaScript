8f5c274199ae38bb839753519e8e2ec5109e3bcf
André Matos <andrermatos90@gmail.com>
2023-07-27 14:20:20 -0300

    javascript moderno

---
+++ CFBCursos/Aula 03 - Diferença entre LET, VAR e CONST/script.js
@@ -0,0 +1,17 @@
+"use strict"
+
+if(true){
+    var nome = "andré"; // var permite acessar a variável pois é "içada" para o topo do escopo
+}
+
+console.log(nome)
+
+/*
+"use strict"
+
+if(true){
+    let nome = "andré"; // let permite acessar a variável somente no bloco em que foi delclarada 
+}
+
+console.log(nome)  // daria erro por que a variável não esta desclara no mesmo escopo
+*/
