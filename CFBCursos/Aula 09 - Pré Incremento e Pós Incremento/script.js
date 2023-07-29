a3b848f6957aa53667e8469a2848bd3fda7221cb
André Matos <andrermatos90@gmail.com>
2023-07-29 11:43:28 -0300

    pré-incremento  e pós-incremento

---
+++ CFBCursos/Aula 09 - Pré Incremento e Pós Incremento/script.js
@@ -0,0 +1,6 @@
+let n = 10;
+
+console.log(n++); //irá imprimir "10" pq o valor só sera incrmentado depois;
+console.log(n); //irá imprimir  "11" pra agora sim foi incrementado
+
+//se no primeiro console fosse ++n ja imprimiria "11"
