12255f1a102a2ef5614f51ff7c82f9d2fc262578
André Matos <andrermatos90@gmail.com>
2023-07-31 14:11:14 -0300

    operador unário typeof

---
+++ CFBCursos/Aula 11 - Operador Typeof/script.js
@@ -0,0 +1,9 @@
+let valor_1 = 10;
+let valor_2 = '10';
+let valor_3 = valor_1 === valor_2;
+let valor_4 = {nome : 'andré'};
+
+console.log(typeof(valor_1));
+console.log(typeof(valor_2));
+console.log(typeof(valor_3));
+console.log(typeof(valor_4));
