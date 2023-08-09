33e742aae330b655b1f27a3711c093ca81050cf1
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-08 21:03:41 -0300

    projeto compra de pizzas parte 1

--- Curso B7web/Módulo 11 - Projeto -Compra de Pizzas/Compra de pizzas - parte 2/pizza_base/pizzas.js
+++ Curso B7web/Módulo 11 - Projeto -Compra de Pizzas/Compra de pizzas - parte 2/pizza_base/pizzas.js
@@ -1,3 +1,6 @@
+//json que simula uma lista de pizza requisitada de um wenservice por exemplo
+
+
 let pizzaJson = [
     {id:1, name:'Mussarela', img:'images/pizza.png', price:20.19, sizes:['100g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
     {id:2, name:'Calabresa', img:'images/pizza2.png', price:18.00, sizes:['320g', '530g', '860g'], description:'Descrição da pizza em mais de uma linha muito legal bem interessante'},
