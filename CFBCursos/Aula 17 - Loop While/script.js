336b0203da41ed9720faaa0beb2aaccd6d8f2fb2
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-10 17:16:54 -0300

    while

---
+++ CFBCursos/Aula 17 - Loop While/script.js
@@ -0,0 +1,22 @@
+console.log("Exemplo uso do while")
+
+let numero = 0;
+
+while (numero <= 10) {
+    console.log(numero);
+    numero++;
+}
+
+let n  = 5;
+fatorial = 1;
+
+
+console.log("Calculo Fatorial")
+while(n >= 1){
+    fatorial = fatorial * n;
+    console.log(n)
+    n--;
+    
+}
+
+console.log(fatorial)
