fe5f82f9d77d7c51117c64ca6ef35c47c45969bc
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-12 21:21:20 -0300

    ex 080 - correção de bug

---
+++ Exercicios/js/ex 080.js
@@ -0,0 +1,15 @@
+/*
+ exemplo simples de um site em HTML com menus e elementos, e vou adicionar um pequeno "bug" de propósito para você resolver. Lembre-se de que a finalidade de aprender a programar é resolver problemas, então tente identificar e corrigir o bug por conta própria. Aqui está o código: */
+
+// Simulando um bug no JavaScript
+document.addEventListener('DOMContentLoaded', function() {
+  const bugElement = document.querySelector('body');
+  bugElement.addEventListener('click', function() {
+    alert("Bug encontrado! Clique OK para resolver o bug.");
+  });
+});
+
+
+/*Você conseguiu encontrar o bug e corrigi-lo! No entanto, o código que você adicionou substituiu o bug original que eu havia inserido. Seu código agora anexa um evento de clique ao elemento body do documento. Isso significa que sempre que você clicar em qualquer parte do corpo da página, o alerta será acionado.
+
+Isso é ótimo! Resolver problemas e depurar são habilidades importantes na programação. Se você tiver mais perguntas ou precisar de ajuda adicional, sinta-se à vontade para perguntar!*/
