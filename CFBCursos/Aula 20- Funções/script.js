e6dba459494d1652f7040f8a2dc1369483000c19
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-14 18:13:08 -0300

    funções

---
+++ CFBCursos/Aula 20- Funções/script.js
@@ -0,0 +1,18 @@
+elemento1 = document.getElementById('c1');
+elemento2 = document.getElementById('c2');
+elemento3 = document.getElementById('c3');
+elemento4 = document.getElementById('c4');
+elemento5 = document.getElementById('c5');
+elemento6 = document.getElementById('c6');
+
+
+
+function alteraTexto(){
+ elemento1.innerHTML = 'alterado';
+ elemento2.innerHTML = 'alterado';
+ elemento3.innerHTML = 'alterado';
+ elemento4.innerHTML = 'alterado';
+ elemento5.innerHTML = 'alterado';
+ elemento6.innerHTML = 'alterado'
+
+}
