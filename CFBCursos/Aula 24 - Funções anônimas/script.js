da0d42665c7dbb1f5e2fd5521c6a34b91761c576
André Matos <103394184+andrematos90@users.noreply.github.com>
2023-08-21 16:34:46 -0300

    Funções anonimas - Aula 25

---
+++ CFBCursos/Aula 24 - Funções anônimas/script.js
@@ -0,0 +1,19 @@
+let s = function(v1, v2) {
+    return v1 + v2;
+}
+
+console.log(s(10, 5));
+
+/*
+função construtor 
+const s = new("v1", "v2", "return V1 + v2")
+
+console.log(s(10, 5));
+
+
+onde v1", "v2" são os argumentos e  "return V1 + v2" é o corpo da função que é o que a função faz 
+o ultimo parametro sempre será o corpo da função
+
+
+
+*/
