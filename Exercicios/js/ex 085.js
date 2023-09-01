9371db05469b83333452488d2e187498b9f52c18
André Matos <andrermatos90@gmail.com>
2023-09-01 17:19:43 -0300

    ex 085 - loop for

---
+++ Exercicios/js/ex 085.js
@@ -0,0 +1,22 @@
+/*Você foi designado para criar um programa em JavaScript que calcule a soma dos números inteiros de 1 a N, onde N é um número inteiro fornecido pelo usuário. O programa deve realizar as seguintes tarefas:
+
+Solicite ao usuário que insira um número inteiro positivo N.
+
+Use um loop for para calcular a soma de todos os números inteiros de 1 a N.
+
+Exiba a soma calculada no console.
+
+*/
+
+
+
+let number = parseInt(prompt("enter a number: "));
+let sum = 0;
+
+for(let i = 0; i <= number; i++){
+    sum += i;
+    
+}
+
+console.log(sum);
+
