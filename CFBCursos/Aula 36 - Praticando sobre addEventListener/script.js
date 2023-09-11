1d153b7c4f26cf2bcea9bad47bbc2d913018cc80
André Matos <andrermatos90@gmail.com>
2023-09-11 15:52:12 -0300

    Aula 36 - praticando AddEventListener

---
+++ CFBCursos/Aula 36 - Praticando sobre addEventListener/script.js
@@ -0,0 +1,41 @@
+let divEsquerda = document.querySelectorAll('.esquerda .exemplo');
+let divDireita = document.querySelector(".direita");
+let botaoCopia = document.querySelector('#botao');
+
+
+//coneverte o HTMLCollection ou nodelist para array
+const arrayDeDivs = Array.from(divEsquerda);
+
+
+arrayDeDivs.map((elemento) =>{
+    elemento.addEventListener("click", (evento)=>{
+        const elementoDiv = evento.target
+        elementoDiv.classList.toggle("selecionada");
+        
+         
+    })
+})
+
+
+botaoCopia.addEventListener("click", transferir);
+
+function transferir(){
+    const divsSelecionadas = [...document.querySelectorAll(".selecionada")]
+    const divsNãoSelecionadas = [...document.querySelectorAll(".exemplo:not(.selecionada)")]
+   
+
+    divsSelecionadas.map((el)=>{
+    el.classList.remove("selecionada");
+    divDireita.appendChild(el)
+    });
+
+    
+    divsNãoSelecionadas.map((el) => {
+        divEsquerda.appendChild(el.cloneNode(true)); // Clonar o elemento antes de mover
+        el.remove(); // Remover o elemento da div direita original
+    });
+}
+    
+
+
+
