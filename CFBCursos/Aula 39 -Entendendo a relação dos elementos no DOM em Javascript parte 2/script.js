eb575f210a5a559e6b55b7be4ab06a43a3fdcea0
André Matos <andrermatos90@gmail.com>
2023-09-12 17:03:21 -0300

    Aula 39 - entende a relação entre os elementos do DOM

---
+++ CFBCursos/Aula 39 -Entendendo a relação dos elementos no DOM em Javascript parte 2/script.js
@@ -0,0 +1,13 @@
+let todaAsDivs = [...document.querySelectorAll(".divs")];
+let body = document.querySelector("body");
+
+
+//método para verificar se elemento tem ou nao filho
+console.log(todaAsDivs[0].hasChildNodes())
+
+console.log(body.firstChild)
+
+
+
+
+
