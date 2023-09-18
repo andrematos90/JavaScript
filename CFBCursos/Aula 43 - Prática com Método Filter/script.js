b7488fcb213e47fe89fd9b7df86773937a8ced10
André Matos <andrermatos90@gmail.com>
2023-09-18 20:51:35 -0300

    ..

--- CFBCursos/Aula 43 - Prática com Método Filter/script.js
+++ CFBCursos/Aula 43 - Prática com Método Filter/script.js
@@ -20,4 +20,28 @@ console.log(menorDeIdade)
 
 
 
+const caixaCursos = document.querySelector('#caixaCursos');
+const btnC = [...document.querySelectorAll(".curso")];
+const c1_2 = document.querySelector('#c1_2');
+const cursos = ['HTML', 'CSS', 'JavaScript', "C#", "ASP.NET", "React"];
+const btnCursoSelecionado = document.getElementById("btnCursoSelecionado");
 
+
+cursos.map((elemento, index) =>{
+    const novoElemento = document.createElement("div");
+    novoElemento.setAttribute("id", "c" + index);
+    novoElemento.setAttribute("class", "curso c1");
+    novoElemento.innerHTML = elemento;
+
+    const comnandos = document.createElement("div");
+    comnandos.setAttribute("class", "comandos")
+
+    const rb= document.createElement("input");
+    rb.setAttribute("type", "radio");
+    rb.setAttribute("name", "rb_curso");
+
+    comnandos.appendChild(rb);
+
+    novoElemento.appendChild(comnandos);
+    caixaCursos.appendChild(novoElemento)
+})
